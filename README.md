69cd4ffc6e9f3fc1e9e7f74f6ddbb1e1a4ba1e4a
Corina Pelichowski <corina@danitt.com>
2018-12-18 13:40:46 +1000

    Up to creating arrays

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JS-Fundamentals-with-Brad
+
+Brushing up on some fundamentals with Brad Traversy
---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+/* Number
+let number1 = "35"; //no quotes = number, quotes = string
+let number2 = "40"; //adding quotes around the numbers and adding them together concatonates. Results in 3540
+
+alert('My favorite number is ' +number1);
+*/
+
+/* Variables:
+  can contain: letters, numbers, underscores, dollar signs
+  start with a letter
+  case sensitive
+*/
+
+// String
+
+/*Array
+used to store multiple values in a single variable*/
+//let colors = ['red', 'blue' , 'green'];
+//alert(colors[1]);
+
+
+
+
+// Object
